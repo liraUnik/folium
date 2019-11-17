755e0c599b1caaa6d8f4bef0324b60543c3f6a70
liraUnik <liridonsejdiu88@gmail.com>
2019-11-17 11:05:37 +0100

    add stamen Terrain tiles to test

--- map.py
+++ map.py
@@ -4,6 +4,9 @@ import folium
 
 # m = folium.Map(location=[42.3702, 21.1483], zoom_start=22)
 
-m = folium.Map(location=[42.37, 21.1483], zoom_start=22, tiles='Stamen Toner')
+# m = folium.Map(location=[42.37, 21.1483], zoom_start=22, tiles='Stamen Toner')
+
+m = folium.Map(location=[42.37, 21.1483],
+               zoom_start=22, tiles='Stamen Terrain')
 
 m.save('index.htm')
