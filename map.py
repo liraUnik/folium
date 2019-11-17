a63ddd4743d67138c3cbf36f3e7578d48a499a69
liraUnik <liridonsejdiu88@gmail.com>
2019-11-17 12:52:45 +0100

    add marker

--- map.py
+++ map.py
@@ -1,12 +1,22 @@
 import folium
 
+
 # ! create map object
 
 # m = folium.Map(location=[42.3702, 21.1483], zoom_start=22)
 
 # m = folium.Map(location=[42.37, 21.1483], zoom_start=22, tiles='Stamen Toner')
 
+# m = folium.Map(location=[42.37, 21.1483],
+#                zoom_start=22, tiles='Stamen Terrain')
+
+
 m = folium.Map(location=[42.37, 21.1483],
-               zoom_start=22, tiles='Stamen Terrain')
+               zoom_start=22, tiles='Stamen Toner')
+
+
+folium.Marker([42.37, 21.1483], popup='Test').add_to(m)
+
+m.add_child(folium.ClickForMarker(popup='Waypoint'))
 
 m.save('index.htm')
