20248c0411e0ab91dfb4f081776113d2bf734fad
liraUnik <liridonsejdiu88@gmail.com>
2019-11-17 10:33:46 +0100

    tematik map

--- map.py
+++ map.py
@@ -2,6 +2,8 @@ import folium
 
 # ! create map object
 
-m = folium.Map(location=[42.3702, 21.1483], zoom_start=22)
+# m = folium.Map(location=[42.3702, 21.1483], zoom_start=22)
+
+m = folium.Map(location=[42.37, 21.1483], zoom_start=22, tiles='Stamen Toner')
 
 m.save('index.htm')
